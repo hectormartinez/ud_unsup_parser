b17bc14f7ffe14a075e0c75ce283e4fc062dc6ca
Héctor Martínez Alonso <hector@hectoridris.local>
2016-02-17 22:04:34 +0100

    x

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,8 @@
 
-for s in 'verbs neighbors' 'verbs function' 'neighbors' 'verbs' 'function' 'content' 'headrule'
+for s in 'verbs neighbors function''verbs neighbors' 'verbs function' 'neighbors' 'verbs' 'function' 'content' 'headrule'
 do
     cd src
     python udup.py --steps $s
     cd ..
-    perl eval.pl -g data/en-ud-dev.conllu -s src/testout.conllu > "$s.eval"
+    perl eval.pl -g data/en-ud-dev.conllu -s src/testout.conllu > "$s.nocomp.eval"
 done
