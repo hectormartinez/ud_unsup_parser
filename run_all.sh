34f06033d808f03b55032036e444c90b2802eedc
Hector <hector.martinez-alonso@inria.fr>
2016-02-18 14:16:27 +0100

    run

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,17 @@
 for s in 'verbs neighbors function' 'verbs neighbors' 'verbs function' 'neighbors' 'verbs' 'function' 'content' 'headrule'
 do
     cd src
+    python udup.py --steps $s --reverse
+    cd ..
+    perl eval.pl -g data/en-ud-dev.conllu -s src/testout.conllu > "$s.pers.reverse.eval"
+done
+
+
+
+for s in 'verbs neighbors function' 'verbs neighbors' 'verbs function' 'neighbors' 'verbs' 'function' 'content' 'headrule'
+do
+    cd src
     python udup.py --steps $s
     cd ..
-    perl eval.pl -g data/en-ud-dev.conllu -s src/testout.conllu > "$s.nocomp.eval"
+    perl eval.pl -g data/en-ud-dev.conllu -s src/testout.conllu > "$s.pers.norev.eval"
 done
