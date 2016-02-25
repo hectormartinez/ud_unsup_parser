e0ca2f4ac944b80e091a3f380ab915dc028aaced
Hector <hector.martinez-alonso@inria.fr>
2016-02-25 15:38:34 +0100

    2tag

--- run_all_pagerank.sh
+++ run_all_pagerank.sh
@@ -17,7 +17,7 @@ s='headrule twotags' #'verbs neighbors function headrule' 'verbs neighbors' 'ver
 for lang in 'en' 'ar' 'cu' 'bg' 'cs' 'da' 'de' 'el' 'et' 'es' 'eu' 'fa' 'fi' 'fr' 'ga' 'got' 'grc' 'he' 'hi' 'hr' 'hu' 'id' 'it' 'la' 'nl' 'no' 'pl' 'pt' 'ro' 'sl' 'sv' 'ta'
 do
     cd src
-    python udup.py --steps $s --input ../data/orgtok/$postype/$lang-ud-test.conllu.lex --lang $lang
+    python udup.py --steps $s --input ../data/orgtok/$postype/$lang-ud-test.conllu --lang $lang
     cd ..
     perl eval07.pl -g data/orgtok/goldpos/$lang-ud-test.conllu -s src/$lang"_testout.conllu" > "$lang.$2tags.eval"
 done
