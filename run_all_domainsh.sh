95f8651e3dcb42b068d0798a7f620d2787fb0a16
Héctor Martínez <hector.martinez-alonso@inria.fr>
2016-05-02 10:32:29 +0200

    domain split

---
+++ run_all_domainsh.sh
@@ -0,0 +1,15 @@
+
+#Best system so far is only headrules
+
+postype="goldpos"
+
+
+s='headrule'
+for domain in 'bg_legal' 'bg_literature' 'bg_news' 'bg_predisentbulletin' 'bg_various' 'en_answers' 'en_bib50' 'en_email' 'en_newsgroup' 'en_questions' 'en_reviews' 'en_watch_56' 'en_weblog' 'hr_news' 'hr_wiki' 'it_europarl' 'it_newslegal' 'it_newspaper' 'it_questions' 'it_various' 'it_wiki' 'sr_news' 'sr_wiki'
+do
+    cd src
+    python udup.py --steps $s --input ../data/orgtok/$postype/$domain-ud-test.conllu --lang $domain
+    cd ..
+    perl eval07.pl -g data/orgtok/domains/$domain-ud-test.conllu -s src/$domain"_testout.conllu" > "$domain.domains.eval"
+done
+
